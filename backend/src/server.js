1844345aad45199d69f8d065ebd87083d9928ecb
VRMX2 <lehcengrissi@gmail.com>
2025-08-13 04:10:50 -0700

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,25 @@ import "dotenv/config";
 import cors from "cors";
 import {ENV} from "./config/env.js";
 import {connectDB} from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
 
 
 const app = express();
 
 
-app.use(express.json({limit:"10mb"}));
+app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+
 
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({status: "ok"});
 });
+
+
+app.use("/api/users",userRoutes);
 
 
 
